3b9f9f1bbe5c5b5d6e9924825e1a1d4a378dd421
Sherman97 <57146492+Sherman97@users.noreply.github.com>
2026-02-12 12:00:19 -0500

    Feature/docker configuration (#14)
    * feat, se creo peticiones y consultas para CRUD en servicio de locations
    
    * feature, creacion de configuracion del docker y AI_workflow
    
    * feature, creacion de configuracion del docker y AI_workflow

--- Backend/services/inventory-service/src/server.js
+++ Backend/services/inventory-service/src/server.js
@@ -2,4 +2,7 @@ require("dotenv").config();
 const app = require("./app");
 
 const PORT = process.env.PORT || 3005;
-app.listen(PORT, () => console.log(`✅ inventory-service on http://localhost:${PORT}`));
+
+app.listen(PORT, "0.0.0.0", () =>
+    console.log(`✅ inventory-service on port ${PORT}`)
+);
