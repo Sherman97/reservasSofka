b3009f081245a8b5c6219ef85ed0f7af0c5766a3
Sherman97 <grmanrjas@gmail.com>
2026-02-16 07:00:03 -0500

    feat, cambio de servicios a spring

---
+++ Backend/services/auth-service/src/test/java/com/reservas/sk/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservas.sk.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
